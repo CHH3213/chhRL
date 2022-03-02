4ae9d9e89015af6c5e7867cf943d29aecf666c6b
CHH3213 <1213472983@qq.com>
2022-03-02 11:20:08 +0800

    PPO开始

---
+++ chh_PPO/README.md
@@ -0,0 +1,7 @@
+
+# Proximal Policy Optimization
+近端策略优化(Proximal Policy Optimization，简称 PPO) 是 policy gradient 的一个变形，它是现在 OpenAI 默认的强化学习算法。
+
+## PPO-Penalty(PPO1)
+
+## PPO-Clip(PPO2)
---
+++ chh_PPO/__init__.py
@@ -0,0 +1,10 @@
+# --*--coding: utf-8 --*--
+"""
+Created with IntelliJ PyCharm.
+@Time: 2022/3/2 11:13
+@PROJECT_NAME: chhRL
+@File: __init__.py
+@Author: chh3213
+@Email:
+@Description:
+"""
